747e4a66d7ba65c5df4c0d958b7e8f679827df80
Dhia Aziz <azizwkwkwk@gmail.com>
2018-03-24 03:50:41 +0700

    lingkup

--- assets/js/main-front.js
+++ assets/js/main-front.js
@@ -1,3 +1,24 @@
+//ref https://stackoverflow.com/questions/17534661/make-anchor-link-go-some-pixels-above-where-its-linked-to
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - 100);
+  }
+}
+
+// Captures click events of all <a> elements with href starting with #
+$(document).on('click', 'a[href^="#"]', function(event) {
+  // Click events are captured before hashchanges. Timeout
+  // causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
+
+
 //JQUERY REQUIRED
 $( document ).ready(function() {
     $('.accordion-button').on('click', function(){
@@ -11,4 +32,9 @@ $( document ).ready(function() {
         $(this).next().css("max-height",panelScrollHeight+1);
       }
     });
+
+    // $("html, body").animate({
+    //  scrollTop: $('#'+window.location.hash).offset().top
+    // }, 1000);
+    // window.location.hash='';
 });
